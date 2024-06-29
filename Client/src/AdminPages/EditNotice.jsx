b99fed003c3aba00cfd8998a8a624bf5bc98c8b1
LasandiRandini <randini-im20057@stu.kln.ac.lk>
2024-06-29 08:58:56 +0530

    final

--- Client/src/AdminPages/EditNotice.jsx
+++ Client/src/AdminPages/EditNotice.jsx
@@ -1,7 +1,4 @@
-// import  { useState, useEffect } from 'react';
-// import axios from 'axios';
-// import { Link } from 'react-router-dom';
-// import Adminheader from "../Components/Adminheader";
+
 
 // const EditNotice = () => {
 //   const [notices, setNotices] = useState([]);
